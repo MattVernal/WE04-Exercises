d1e06adef8a19d540a26d985e03659938d771b13
845865636 <845865636@12617-LAB4.christchurch.natcoll.ac.nz>
2016-12-12 12:19:37 +1300

    first commit

---
+++ jQuery_exercises/public_html/js/main.js
@@ -0,0 +1,13 @@
+//----------------------Exercise 1----------------------------------
+//$(function() {
+//    $('#textarea').keyup(function() {
+//        var textLen = ($(this).val()).length;
+//        console.log(textLen);
+//        $('#rem-chars').text(15 - textLen);
+//    })    
+//});
+
+ $('#add-btn').click(function() {
+    $('#fruit-list').append('<li>Banana</li>');  
+  });
+
