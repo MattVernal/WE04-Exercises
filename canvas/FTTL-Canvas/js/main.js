8a7095b36b33a2f658dc5ce63fe0514563d7ed5c
845865636 <845865636@12617-LAB4.christchurch.natcoll.ac.nz>
2016-12-13 15:45:51 +1300

    Started a mini project using fabric.js on my FTTL services page

---
+++ canvas/FTTL-Canvas/js/main.js
@@ -0,0 +1,11 @@
+(function(){
+    var canvas = new fabric.Canvas('c');
+    var imgElement = document.getElementById('my-image');
+    var imgInstance = new fabric.Image(imgElement, {
+        left: 100,
+        top: 100,
+//        angle: 30,
+        opacity: 0.85
+    });
+    canvas.add(imgInstance);
+})();
--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,13 +1,11 @@
 function draw() {
-    var canvas = document.getElementById('tutorial');
-    if (canvas.getContext) {
-        //2 Overlapping Squares
-//        var ctx = canvas.getContext('2d');
-//        ctx.fillStyle = "rgb(200,0,0)";
-//        ctx.fillRect (10, 10, 50, 50);
-//
-//        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-//        ctx.fillRect (30, 30, 50, 50);
-          
-    }
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    //Draw a square
+//    ctx.fillRect(25,25,100,100);
+//    ctx.clearRect(45,45,60,60);
+//    ctx.strokeRect(50,50,50,50);
+
+  }
 }
